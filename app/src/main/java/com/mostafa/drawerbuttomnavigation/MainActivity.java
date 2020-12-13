bea11de46da7e5f3e456c6c1635e231c3994796c
Sirajul Mostafa <sirajulmost@gmail.com>
2020-12-13 21:45:48 +0600

    first commit

---
+++ app/src/main/java/com/mostafa/drawerbuttomnavigation/MainActivity.java
@@ -0,0 +1,76 @@
+package com.mostafa.drawerbuttomnavigation;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.Menu;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private BottomNavigationView bottomNavigationView;
+    AppBarConfiguration appBarConfiguration;
+
+    private  NavController navController;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);//drawerNavigation  menu
+        bottomNavigationView = findViewById(R.id.bottom_nav_view);
+
+       appBarConfiguration =
+               // new AppBarConfiguration.Builder(navController.getGraph())
+                new AppBarConfiguration.Builder( R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,R.id.nav_setting,R.id.shareFragment)
+                     //   .setDrawerLayout(drawerLayout)
+                        .setOpenableLayout(drawerLayout)
+                        .build();
+
+//        NavigationUI.setupActionBarWithNavController(this,navController);
+        navController = Navigation.findNavController(this,R.id.nav_host_fragment);
+        NavigationUI.setupActionBarWithNavController(this,navController,appBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);//for drawer nav
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);//for bottom nav
+
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+         navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START))
+            drawerLayout.closeDrawer(GravityCompat.START);
+        else
+        super.onBackPressed();
+    }
+}
